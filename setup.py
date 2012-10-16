9476987ebaa5a6a9969136ee2da66a411a903008
Patrick Altman <paltman@gmail.com>
2012-10-15 22:18:27 -0500

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import setup, find_packages
 
 
@@ -7,16 +6,15 @@ VERSION = __import__("pybrightcove").__version__
 
 setup(
     name="pybrightcove",
-    version = VERSION,
+    version=VERSION,
     author="Patrick Altman",
-    author_email="patrick@studionow.com",
-    url="git://github.com/studionow/pybrightcove.git",
+    author_email="paltman@eldarion.com",
+    url="http://github.com/studionow/pybrightcove/",
     description="""A Python interface for the Brightcove APIs""",
     packages=find_packages(),
-    namespace_packages = [],
-    include_package_data = True,
+    namespace_packages=[],
+    include_package_data=True,
     zip_safe=False,
     license="MIT",
     install_requires=["requests==0.14.1"]
 )
-
