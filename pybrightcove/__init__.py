90ba7e70e381db841422e7d175343df0533ba8a3
Patrick Altman <paltman@gmail.com>
2012-10-15 22:20:17 -0500

    1.4.0.dev1

--- pybrightcove/__init__.py
+++ pybrightcove/__init__.py
@@ -18,26 +18,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-"""
-pybrightcove is a simple wrapper around the Brightcove Media API
-"""
-
-VERSION = (1, 3, 7, "f") # , "a", 1) # following PEP 386
-DEV_N = 1
-
-
-def get_version():
-    version = "%s.%s" % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = "%s.%s" % (version, VERSION[2])
-    if VERSION[3] != "f":
-        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
-        if DEV_N:
-            version = "%s.dev%s" % (version, DEV_N)
-    return version
-
-
-__version__ = get_version()
-
-
-
+__version__ = "1.4.0.dev1"
